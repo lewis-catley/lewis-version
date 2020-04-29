04fb375cbfa52141224607102a3ff14cd3d57285
Lewis Catley <lewis.catley@emishealth.com>
2020-04-29 14:46:43 +0100

    0.0.2 Release

--- hello/hello.go
+++ hello/hello.go
@@ -5,5 +5,5 @@ import "fmt"
 // PrintVimGo will print `vim-go` 
 func PrintVimGo() {
 	fmt.Println("Hello World!")
-	fmt.Println("Tagged release version 0.0.1")
+	fmt.Println("Tagged release version 0.0.2")
 }
